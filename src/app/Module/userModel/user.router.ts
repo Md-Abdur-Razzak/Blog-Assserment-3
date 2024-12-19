bd87375bde1269126368ddd5460996d6b1e00b61
Md-Abdur-Razzak <razzak445444@gmail.com>
2024-12-19 14:37:57 +0600

    userModle defined

---
+++ src/app/Module/userModel/user.router.ts
@@ -0,0 +1,16 @@
+import express from "express"
+const router = express.Router
+
+router.post('/products',);
+// router.get('/products', );
+// router.get('/products/:productId', );
+// router.put('/products/:productId', );
+// router.delete('/products/:productId',);
+
+// router.post('/products',);
+// router.get('/products', );
+// router.get('/products/:productId', );
+// router.put('/products/:productId', );
+// router.delete('/products/:productId',);
+
+export default router;
